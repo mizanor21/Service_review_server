b8e0581a89fc4a15793f45334379845798036c75
mizanor21 <mizanorrahman398@gmail.com>
2022-11-08 04:44:36 +0600

    database connection code added

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+require('dotenv').config();
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.disah5t.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const foodCollection = client.db('hungryNaki').collection('foodItems');
+
+        // app.post('/', async (req, res) => {
+        // })
+        const item = {
+            foodTitle: 'Birany',
+            price: 150,
+            description: 'Awesome birany!!'
+        }
+        const result = await foodCollection.insertOne(item);
+        res.send(result);
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hungry naki node server running!');
